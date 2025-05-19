0fb63e325ab6cc7f0d98597206cca615ec6e6785
Bedrov <Nikolay.Bedrov@iconplc.com>
2025-05-19 13:25:36 +0300

    Improved the api request script. Made the script use parametrs and automated the process for multiple drugs

--- src/main.py
+++ src/main.py
@@ -7,9 +7,15 @@ import requests
 from urllib.parse import quote
 # rate limiting is important to avoid accidental service abuse of the OpenFDA API provider
 from ratelimit import limits, sleep_and_retry
+from munch import Munch
+from enum import Enum
+from collections import defaultdict
 
+#FDA API constants
 OPENFDA_API = "https://api.fda.gov/drug/event.json"
+OPENFDA_METADATA_YAML = "https://open.fda.gov/fields/drugevent.yaml"
 
+#FDA API functions
 @sleep_and_retry
 @limits(calls=40, period=60)
 def call_api(params):
@@ -27,6 +33,10 @@ def call_api(params):
     params['limit'] = params.get('limit', 1000)
     response = requests.get(OPENFDA_API, params=params)
 
+    if response.status_code == 404:
+        print('API response: {}'.format(response.status_code))
+        print(f'No results found for the given search term:\n {params}')
+        return []
     if response.status_code != 200:
         raise Exception('API response: {}'.format(response.status_code))
     return response.json()['results']
@@ -53,11 +63,6 @@ def call_api_raw_result(params):
 
     return response.json()
 
-
-OPENFDA_METADATA_YAML = "https://open.fda.gov/fields/drugevent.yaml"
-
-from munch import Munch
-
 def api_meta():
     """
     YAML file with field description and other metadata retrieved from the OpenFDA website
@@ -71,134 +76,157 @@ def api_meta():
     y = Munch.fromYAML(response.text)
     return y['properties']
 
-######### Drug Name ##########
-#drugname = "NEXLETOL"
-#drugname = "NEXLIZET"
-#drugname = "AKTEN"
+class DrugNameType(Enum):
+    """
+    Enum for drug name type
+    """
+    MEDICINAL_PRODUCT = 1
+    GENERIC_NAME = 2
 
-drugname = 'exenatide'
-#drugname = 'LIRAGLUTIDE'
-#drugname = 'LIXISENATIDE'
-#drugname = 'ALBIGLUTIDE'
-#drugname = 'DULAGLUTIDE'
-#drugname = 'SEMAGLUTIDE'
-#drugname = 'BEINAGLUTIDE'
+api_cache = {}
+
+#FDA API Calls
+
+def get_all_cases_for_drug_per_year(drugname, year, drugname_type=DrugNameType.MEDICINAL_PRODUCT):
+    if drugname_type == DrugNameType.MEDICINAL_PRODUCT:
+        return call_api({
+            "count": "patient.drug.medicinalproduct.exact",
+            'search':f'patient.drug.medicinalproduct:"{drugname}" AND receivedate:[{year}0101 TO {year}1231]'
+        })
+    elif drugname_type == DrugNameType.GENERIC_NAME:
+        return call_api({
+            "count": "patient.drug.openfda.generic_name.exact",
+            'search':f'patient.drug.openfda.generic_name:"{drugname}" AND receivedate:[{year}0101 TO {year}1231]'
+        })
+    else:
+        raise ValueError('Invalid drug name type')
 
 
-faers_all_cases = call_api_raw_result({"limit": 1})['meta']['results']['total']
-
-
-
-
-
-
-
-
-
-
-
-
-#If necessary add the time frame - 'search':'receivedate:[20040101 TO 20200101] AND ...'
-#GET AB - all cases for drug
-
-# df = pd.DataFrame(call_api({
-#     "count": "patient.drug.medicinalproduct.exact",
-#     'search':'patient.drug.medicinalproduct:"{}"'.format(drugname)
-#     }))
-
-#Break down the data by year due to api limits (1000 records per call)
-start_year = 2005
-end_year = 2025
-
-all_data = []
-
-for year in range(start_year, end_year + 1):
-    #print(year)
-    data = call_api({
-        "count": "patient.drug.openfda.generic_name.exact",
-        'search':f'patient.drug.openfda.generic_name:"{drugname}" AND receivedate:[{year}0101 TO {year}1231]'
-    })
+def get_drug_event_count_A_per_year(drugname, year, drugname_type=DrugNameType.MEDICINAL_PRODUCT):   
+    if drugname_type == DrugNameType.MEDICINAL_PRODUCT:
+        return call_api({
+            "count": "patient.reaction.reactionmeddrapt.exact",
+            'search':f'patient.drug.medicinalproduct:"{drugname}" AND receivedate:[{year}0101 TO {year}1231]'
+        })
+    elif drugname_type == DrugNameType.GENERIC_NAME:
+        return call_api({
+            "count": "patient.reaction.reactionmeddrapt.exact",
+            'search':f'patient.drug.openfda.generic_name:"{drugname}" AND receivedate:[{year}0101 TO {year}1231]'
+        })
+    else:
+        raise ValueError('Invalid drug name type')
     
-    all_data.extend(data)
 
-df = pd.DataFrame(all_data)
+def add_items(new_items, combined):
 
-
-#drug_AB = df[df["term"] == drugname]["count"].values[0]
-
-test_new_AB_count = df[df['term'].str.contains(drugname, case=False, na=False)]['count'].sum()
-
-
-
-
-
+    term_to_index = {item['term']: idx for idx, item in enumerate(combined)}
+    for item in new_items:
+        term = item['term']
+        count = item['count']
+        if term in term_to_index:
+            combined[term_to_index[term]]['count'] += count
+        else:
+            combined.append({'term': term, 'count': count})
 
 
+def get_all_cases_for_drug_A(drugname, start_year, end_year, drugname_type=DrugNameType.MEDICINAL_PRODUCT):
+    
+    all_years_data = []
+
+    for year in range(start_year, end_year + 1):
+        data = get_drug_event_count_A_per_year(drugname, year, drugname_type)
+
+        add_items(data, all_years_data)
+
+    df = pd.DataFrame(all_years_data)
+    #count = df[df['term'].str.contains(drugname, case=False, na=False)]['count'].sum()
+    
+    return df
+
+
+def get_all_cases_for_drug_AB(drugname, start_year, end_year, drugname_type=DrugNameType.MEDICINAL_PRODUCT):
+    
+    all_years_data = []
+
+    for year in range(start_year, end_year + 1):
+        data = get_all_cases_for_drug_per_year(drugname, year, drugname_type)
+        all_years_data.extend(data)
+
+    df = pd.DataFrame(all_years_data)
+    count = df[df['term'].str.contains(drugname, case=False, na=False)]['count'].sum()
+    
+    return count
 
 
 
-
-
-
-
-
-
-
-
-#GET A - all cases for drug + event 
-df = pd.DataFrame(call_api({
-    "count": "patient.reaction.reactionmeddrapt.exact",
-    'search':'patient.drug.medicinalproduct:"{}"'.format(drugname)
-    }))
-
-drug_event_AB = df
-
-reactions = df['term'].values
-reactions_counts = []
 
 def get_counts_for_reaction(reaction):
-    """Returns dataframe with yearly tally of event reports for a given reaction"""
-    aes_df = pd.DataFrame(call_api({
-            "count": "patient.reaction.reactionmeddrapt.exact",
-            'search':'patient.reaction.reactionmeddrapt:"{}"'.format(reaction.replace("^", " ").replace("/", " "))
-    }))
- 
-    for item in aes_df.iterrows():
-        if item[1]["term"].lower() == reaction.lower():
-            reactions_counts.append([reaction, item[1]["count"]])
-            print("Added ", reaction.lower())
+        """Returns dataframe with yearly tally of event reports for a given reaction"""
+        aes_df = pd.DataFrame(call_api({
+                "count": "patient.reaction.reactionmeddrapt.exact",
+                'search':'patient.reaction.reactionmeddrapt:"{}"'.format(reaction.replace("^", " ").replace("/", " "))
+        }))
     
+        for item in aes_df.iterrows():
+            if item[1]["term"].lower() == reaction.lower():
+                return item[1]["count"]
+          
+                
 
-for reaction in reactions:
-    get_counts_for_reaction(reaction)
+#Current run parameters
+start_year = 2005
+end_year = 2025
+drug_names = ["exenatide","liraglutide","lixisenatide","albiglutide","dulaglutide","semaglutide","beinaglutide"]
+drug_type = DrugNameType.GENERIC_NAME
+
+if __name__ == "__main__":
     
-drug_AC = dict(reactions_counts)
+    all_cases_ABCD = call_api_raw_result({"limit": 1})['meta']['results']['total']
 
-aedata = {}
+    for drug_name in drug_names:
 
-stats_df = pd.DataFrame(columns=["Drug Name", "PT Name", "A", "AB", "AC", "ABCD"])
+        all_cases_for_drug_AB_count = get_all_cases_for_drug_AB(drug_name, start_year, end_year, drug_type)
 
-for index, drugevent in enumerate(drug_event_AB.values):
-    ac_count = drug_AC.get(drugevent[0]) 
-    row = (drugname, drugevent[0], drugevent[1], drug_AB, ac_count, faers_all_cases )
-    stats_df.loc[index] = row
+        #GET A - all cases for drug + event 
+        drug_event_A = pd.DataFrame(get_all_cases_for_drug_A(drug_name, start_year, end_year, drug_type))
 
-stats_df.to_csv(f"FAERS_{drugname}.csv", index=False)
+        reactions = drug_event_A['term'].values
+        #reactions = drug_event_A['term'].values[:20]
+
+        reactions_counts = []
+
+        for reaction in reactions:
+
+            cache_key = reaction
+            if cache_key in api_cache:
+                reaction_total_count = api_cache[cache_key]
+                reactions_counts.append([reaction, reaction_total_count])
+                print("Added from cache", reaction.lower())
+            else:
+                reaction_total_count = get_counts_for_reaction(reaction)
+                reactions_counts.append([reaction, reaction_total_count])
+                api_cache[cache_key] = reaction_total_count
+                print("Added from API", reaction.lower())
+
+            
+        drug_AC = dict(reactions_counts)
 
 
+        stats_df = pd.DataFrame(columns=["Drug Name", "PT Name", "A", "AB", "AC", "ABCD"])
 
+        for index, drugevent in enumerate(drug_event_A.values):
+            ac_count = drug_AC.get(drugevent[0]) 
+            row = (drug_name, drugevent[0], drugevent[1], all_cases_for_drug_AB_count, ac_count, all_cases_ABCD )
+            stats_df.loc[index] = row
 
-
-
-
-
-print("debug")
-
-
-
+        stats_df.to_csv(f"FAERS_{drug_name}.csv", index=False)
 
 
 #Sort out this script 
 #1. Make the api request type optional - medicinal product or generic name
 #2. Make the count for the AB iterate all years.
 #3. Cleanup the code and make it reusable.
+
+
+
+
